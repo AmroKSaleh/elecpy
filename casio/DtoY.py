8788bca055d28dad7fd889149bf15f1f867cd61f
AmroKSaleh <amro@jameedium.org>
2022-04-04 22:21:14 +0300

    add complex value input support

--- casio/DtoY.py
+++ casio/DtoY.py
@@ -1,11 +1,28 @@
 transType = int(input("D To Y 0|Y To D 1"))
+varType = int(input("Real 0|Complex 1"))
 
 if transType:
     # Y to D
     print("Draw a Y shape")
-    r1 = float(input("left   \ R1:"))
-    r2 = float(input("right  / R2:"))
-    r3 = float(input("bottom | R3:"))
+    if varType:
+        # Complex
+        # R1
+        r1r = float(input("left   \ R1 r:"))
+        r1c = float(input("left   \ R1 c:"))
+        r1 = complex(r1r, r1c)
+        # R2
+        r2r = float(input("right  / R2 r:"))
+        r2c = float(input("right  / R2 c:"))
+        r2 = complex(r2r, r2c)
+        # R3
+        r3r = float(input("bottom | R3 r:"))
+        r3c = float(input("bottom | R3 c:"))
+        r3 = complex(r3r, r3c)
+    else:
+        # Real
+        r1 = float(input("left   \ R1:"))
+        r2 = float(input("right  / R2:"))
+        r3 = float(input("bottom | R3:"))
 
     print("Draw upside triangle")
     # calculate r
@@ -16,10 +33,26 @@ if transType:
 else:
     # D to Y
     print("Draw upside triangle")
-    ra = float(input("right / Ra:"))
-    rb = float(input("left  \ Rb:"))
-    rc = float(input("top   - Rc:"))
-
+    if varType:
+        # Complex
+        # Ra
+        rar = float(input("right   / Ra r:"))
+        rac = float(input("right   / Ra c:"))
+        ra = complex(rar, rac)
+        # Rb
+        rbr = float(input("left  \ Rb r:"))
+        rbc = float(input("left  \ Rb c:"))
+        rb = complex(rbr, rbc)
+        # Rc
+        rcr = float(input("top - Rc r:"))
+        rcc = float(input("top - Rc c:"))
+        rc = complex(rcr, rcc)
+    else:
+        # Real
+        ra = float(input("right  / Ra:"))
+        rb = float(input("left  \ Rb:"))
+        rc = float(input("top    - Rc:"))
+    
     print("Draw a Y shape")
     # calculate r1, r2, r3 from ra, rb, rc
     r1 = (rb*rc)/(ra+rb+rc)
